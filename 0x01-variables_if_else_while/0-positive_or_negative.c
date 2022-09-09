f68e51d9d2483a8c97d0a529a699799b6bb87fa5
Ousmane <ousmaneouattara@protonmail.com>
2022-09-09 14:45:53 +0000

    started 0x01 project

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *  main - Do a test with if condition
+ *
+ *  Description: This script test a random number
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is 0\n", n);
+
+	return (0);
+}
