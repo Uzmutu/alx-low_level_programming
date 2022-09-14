52af3bfb748e5c647856ac3590ad0bee3dc22d54
Ousmane <ousmaneouattara@protonmail.com>
2022-09-14 01:25:50 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - to print sum of fibonacci
+ *
+ * Description: print the sum of even number of 50 first fibonacci number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long number_1;
+	unsigned long number_2;
+	unsigned long number_3;
+	unsigned long sum;
+	int i;
+
+	number_1 = 0;
+	number_2 = 1;
+	for (i = 0; i < 4000000; i++)
+	{
+		number_3 = number_1 + number_2;
+		if (number_3 % 2 == 0)
+		{
+			sum = sum + number_3;
+		}
+		number_1 = number_2;
+		number_2 = number_3;
+	}
+	printf("The sum of even number in the 50 first fibonacci tree is %lu\n", sum);
+
+	return (0);
+}
