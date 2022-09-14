615cfdfa9e17afb37b1c4533527387eb8dcc7274
Ousmane <ousmaneouattara@protonmail.com>
2022-09-14 01:54:38 +0000

    fixed.1 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,7 +17,7 @@ int main(void)
 	n1 = 1;
 	n2 = 2;
 	printf("%lu, %lu", n1, n2);
-	for (i = 0; i < 49; i++)
+	for (i = 0; i < 48; i++)
 	{
 		n3 = n1 + n2;
 		printf(", %lu", n3);
@@ -25,7 +25,7 @@ int main(void)
 		n1 = n2;
 		n2 = n3;
 
-		if (i == 48)
+		if (i == 47)
 			printf("\n");
 
 	}
