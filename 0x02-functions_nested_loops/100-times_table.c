cb8160057659f15faebf4d21878b6a4ad76c22f7
Ousmane <ousmaneouattara@protonmail.com>
2022-09-14 01:41:52 +0000

    fixed 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,55 +2,65 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_times_table - print n time table
- * @n: tested variable
+ * space - put some space
  *
- * Description: print all n times table starting to 0
+ * Description: use _putchar to put some some space
+ *
+ * Return: return space
+ */
+void space(void)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+}
+/**
+ * print_times_table - print n time table
+ * @n: argument for calculation
+ *
+ * Description: print all n of given number times table starting to 0
+ * if n greater than 0 and n less than or equal to 15
  *
  * Return: return the table
  */
 void print_times_table(int n)
 {
-	if (n > 0 && n < 15)
+	int a;
+	int k;
+	int v;
+
+	if (n > 0 && n <= 15)
 	{
-		int i;
-
-		for (i = 0; i <= n; n++)
+		for (a = 0; a <= n; a++)
 		{
-			int k;
-			int v;
-
-			for (k = 0; k <= 9; k++)
+			for (k = 0; k <= n; k++)
 			{
-				v = n * k;
-				if (k < 9)
+				v = a * k;
+				if (v <= 9)
 				{
-					if (v <= 9)
+					if (k != 0)
 					{
-						_putchar((v % 10) + '0');
-						_putchar(',');
+						space();
 						_putchar(' ');
 					}
-					else if (v > 9)
-					{
-						_putchar((v / 10) + '0');
-						_putchar((v % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
+					_putchar((v % 10) + '0');
 				}
-				if (k == 9)
+				else if (v <= 99)
 				{
-					if (v < 10)
-						_putchar((v % 10) + '0');
-					else
-					{
-						_putchar((v / 10) + '0');
-						_putchar((v % 10) + '0');
-					}
+					space();
+					_putchar((v / 10) + '0');
+					_putchar((v % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((v / 100) + '0');
+					_putchar(((v / 10) - 10) + '0');
+					_putchar((v % 10) + '0');
 				}
 			}
-			_putchar('\n');
+		_putchar('\n');
 		}
 	}
 }
