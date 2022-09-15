adb9eee81ba77ced93c92a494f5c32f12d10ed9c
Ousmane <ousmaneouattara@protonmail.com>
2022-09-15 22:27:57 +0000

    fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,9 +13,15 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n;
+		n *= -1;
+		if (n / 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	else
+	{
+		if (n / 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
 }
