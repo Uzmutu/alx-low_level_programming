3e8fc2a1aa55cf30f2cc8f377560989d335bd4f0
Ousmane <ousmaneouattara@protonmail.com>
2022-09-15 12:22:21 +0000

    fixed 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (number = 1; number <= 100; number++)
 	{
-		if (number % 15 == 0)
+		if (number % 3 == 0 && number % 5 == 0)
 			printf(" FizzBuzz");
 		else if (number % 3 == 0)
 			printf(" Fizz");
