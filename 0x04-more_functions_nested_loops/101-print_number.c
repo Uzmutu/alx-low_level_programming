10e5c36854cdca6a902f05a70f87117dc5f9a5b8
Ousmane <ousmaneouattara@protonmail.com>
2022-09-16 07:12:02 +0000

    fix 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,12 +10,41 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	int half;
+	int half2;
+
+	if (n < 0 && n > -2147483648)
 	{
 		_putchar('-');
 		n = -n;
+		if (n / 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	else if (n == -2147483648)
+	{
+		half = (n / -1000);
+		half2 = (n % 1000);
+		half2 *= -1;
+		_putchar('-');
+		_putchar((half / 1000000) + '0');
+		_putchar(((half % 1000000) / 100000) + '0');
+		_putchar((((half % 1000000) % 100000) / 10000) + '0');
+		_putchar(((((half % 1000000) % 100000) % 10000) / 1000)  + '0');
+		_putchar((((((half % 1000000) % 100000) % 10000) % 1000) / 100)  + '0');
+		_putchar(((((((half % 1000000) % 100000)
+								% 10000) % 1000) % 100) / 10)  + '0');
+		_putchar((half % 10) + '0');
+		_putchar((half2 / 100) + '0');
+		_putchar(((half2 % 100) / 10) + '0');
+		_putchar((half2 % 10) + '0');
+	}
+	else
+	{
+		if (n / 10)
+		{
+			print_number(n / 10);
+		}
+		_putchar(n % 10 + '0');
+	}
 }
