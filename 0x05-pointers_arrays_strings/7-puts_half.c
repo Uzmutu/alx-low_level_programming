a8b5b1bd794ca76c92ec9e9ae85284d4478d698a
Ousmane <ousmaneouattara@protonmail.com>
2022-09-19 16:53:17 +0000

    add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * puts_half - function that cut in half
+ * @str: pointer of the string
+ *
+ * Description: function function that prints half of a string,
+ * followed by a new line. If the number of characters is odd,
+ * the function should print the last n characters of the string,
+ * where n = (length_of_the_string - 1) / 2
+ *
+ * Return: return string
+ */
+int puts_half(char *str)
+{
+	int length_of_the_string;
+	int half;
+	int count;
+	int n;
+
+	length_of_the_string = 0;
+	while (s[length_of_the_string] != '\0')
+	{
+		length_of_the_string++;
+	}
+	if (length_of_the_string % 2 != 0)
+	{
+		n = (length_of_the_string - 1) / 2;
+		count = (length_of_the_string - n);
+		while (count <= length_of_the_string)
+		{
+			_putchar(str[count]);
+			count++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		half = length_of_the_string / 2;
+		count = half;
+		while (count <= length_of_the_string)
+		{
+			_putchar(str[count]);
+			count++;
+		}
+		_putchar('\n');
+	}
+}
