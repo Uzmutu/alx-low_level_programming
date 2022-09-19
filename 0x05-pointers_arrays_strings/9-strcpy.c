38f0ed164c9f6c77023fd3a8701000eaa83dd968
Ousmane <ousmaneouattara@protonmail.com>
2022-09-19 17:46:41 +0000

    just add 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * *_strcpy - function that copy
+ * @dest: pointer of the copied string destination
+ * @src: pointer of string that copied
+ *
+ * Description: function who copy a given string
+ *
+ * Return: return string dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+
+	count = 0;
+	while (src[count])
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	return (dest);
+
+}
