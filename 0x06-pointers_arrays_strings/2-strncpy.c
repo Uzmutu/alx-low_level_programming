b2f11e3555a413f49c1b9eb22d4c1f8d3940c50b
Ousmane <ousmaneouattara@protonmail.com>
2022-09-21 11:06:36 +0000

    fixed 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int count;
 
-	for (count = 0; count < n; count++)
+	for (count = 0; count < n && src[count] != '\0'; count++)
 		dest[count] = src[count];
+	for ( ; count < n; count++)
+		dest[count] = '\0';
 	return (dest);
 }
