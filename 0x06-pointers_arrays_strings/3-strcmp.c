59634169380595bb73abf205baeaf41a0eaf48de
Ousmane <ousmaneouattara@protonmail.com>
2022-09-22 12:35:18 +0000

    fix 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _strlen - function that count
- * @s: pointer of the string that be counted
- *
- * Description: function who count the lenght of a given string
- *
- * Return: return the lenght
- */
-int _strlen(char *s)
-{
-	int count;
-
-	count = 0;
-	while (*s != '\0')
-	{
-		count++;
-		s++;
-	}
-	return (count);
-}
-/**
  * _strcmp - function that compare
  * @s1: string to compare to s2
  * @s2: string to compare to s1
  *
  * Description: this function compare to string and return an int
  *
- * Return: return 15, -15 or 0
+ * Return: return difference
  */
 int _strcmp(char *s1, char *s2)
 {
-	int length_str_1;
-	int length_str_2;
+	int count;
 
-	length_str_1 = _strlen(s1);
-	length_str_2 = _strlen(s2);
+	for (count = 0; (s1[count] == s2[count]); count++)
+	{
+		if (s1[count] == '\0')
+			return (0);
+	}
+	return (s1[count] - s2[count]);
 
-	if (length_str_1 > length_str_2)
-		return (15);
-	else if (length_str_1 < length_str_2)
-		return (-15);
-	else
-		return (0);
 }
