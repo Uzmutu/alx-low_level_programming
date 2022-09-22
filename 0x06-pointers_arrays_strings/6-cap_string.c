9c780fd379fcf3ce685a3039768fd17c6d4ce846
Ousmane <ousmaneouattara@protonmail.com>
2022-09-22 15:56:36 +0000

    add 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *cap_string - function that capitalize
+ * @s: manipulated string
+ *
+ * Description: function that capitalize all words.
+ *
+ * Return: the changed string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+		}
+		while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+				|| s[i] == '"' || s[i] == ',' || s[i] == '!' ||
+				s[i] == ';' || s[i] == '?' || s[i] == '(' || s[i] == ')'
+				|| s[i] == '{' || s[i] == '}')
+		{
+			++i;
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
