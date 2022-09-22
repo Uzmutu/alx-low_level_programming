2868afaff9886a7e53cff553dfc3ba76b0347463
Ousmane <ousmaneouattara@protonmail.com>
2022-09-22 19:36:09 +0000

    fixed 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,41 +10,17 @@
  */
 void print_number(int n)
 {
-	int half;
-	int half2;
-
-	if (n < 0 && n > -2147483648)
+	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
-		if (n / 10)
-			print_number(n / 10);
-		_putchar(n % 10 + '0');
-	}
-	else if (n == -2147483648)
-	{
-		half = (n / -1000);
-		half2 = (n % 1000);
-		half2 *= -1;
-		_putchar('-');
-		_putchar((half / 1000000) + '0');
-		_putchar(((half % 1000000) / 100000) + '0');
-		_putchar((((half % 1000000) % 100000) / 10000) + '0');
-		_putchar(((((half % 1000000) % 100000) % 10000) / 1000)  + '0');
-		_putchar((((((half % 1000000) % 100000) % 10000) % 1000) / 100)  + '0');
-		_putchar(((((((half % 1000000) % 100000)
-								% 10000) % 1000) % 100) / 10)  + '0');
-		_putchar((half % 10) + '0');
-		_putchar((half2 / 100) + '0');
-		_putchar(((half2 % 100) / 10) + '0');
-		_putchar((half2 % 10) + '0');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
 	}
 	else
 	{
-		if (n / 10)
-		{
+		if (n > 9)
 			print_number(n / 10);
-		}
 		_putchar(n % 10 + '0');
 	}
 }
