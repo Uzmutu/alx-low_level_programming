0ca9d0ca2611b4b61af9694c074250a4c63741c1
Ousmane <ousmaneouattara@protonmail.com>
2022-09-26 11:07:43 +0000

    forget about betty

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - function that search
+ * @s: string where the research is done
+ * @c: the character to research
+ *
+ * Description:  function that locates a character in a string.
+ *
+ * Return: returns a pointer to the first occurrence
+ * of the character c in the string s
+ */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	i = 0;
-	if (*(s + i) == c)
-		return ((s + i));
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
-		if (*(s + i) == c)
+		if (*s == c)
 		{
-			return ((s + i));
+			return (s);
 		}
-		i++;
+		s++;
 	}
 	return (0);
 }
