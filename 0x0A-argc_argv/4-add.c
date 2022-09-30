48621704fec0b6f8498fd00a83acca0d3f1a6b52
Ousmane <ousmaneouattara@protonmail.com>
2022-09-30 06:25:28 +0000

    fix add program

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,12 +17,13 @@ int main(int argc, char *argv[])
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (!(atoi(argv[i])) || atoi(argv[i]) < 0)
+		if (atoi(argv[i]))
+			sum += atoi(argv[i]);
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
