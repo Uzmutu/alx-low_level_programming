5925ddb3f8db23b484bf72a267ee75a33cd08a59
Ousmane <ousmaneouattara@protonmail.com>
2022-09-30 15:50:46 +0000

    fix 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - entry point
  * @argc: count the number of command line arguments
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!atoi(argv[i]))
+			if (isalpha(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
