aace4de4b83c7bac50d6a08efcb8d9d82276055d
Ousmane <ousmaneouattara@protonmail.com>
2022-10-10 21:28:18 +0000

    fixed 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,6 +43,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	n_dog->owner = malloc(sizeof(char) * (o + 1));
 	if (!n_dog->name || !n_dog->owner)
 	{
+		free(n_dog->name);
+		free(n_dog->owner);
 		free(n_dog);
 		return (NULL);
 	}
