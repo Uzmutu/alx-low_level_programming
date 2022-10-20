09608391620bf2f1e6ea81e1c514a63a0c84db62
Ousmane OUATTARA <ousmaneouattara@protonmail.com>
2022-10-20 19:05:50 +0000

    add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - function
+ * @h: pointer to head of list
+ *
+ * Description: function that returns the number of elements
+ * in a linked list_t list.
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	for (i = 0; h; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
